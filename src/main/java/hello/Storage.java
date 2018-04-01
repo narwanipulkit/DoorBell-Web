56526894c8406bff96a9e461f56be4f81a9fcfb6
narwanipulkit <pulkitnarwani@gmail.com>
2018-04-01 17:41:05 +0530

    Database Support to storage

--- src/main/java/hello/Storage.java
+++ src/main/java/hello/Storage.java
@@ -8,9 +8,11 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.StorageClient;
 import com.google.cloud.storage.Bucket;
+import com.google.firebase.database.*;
 
 class Storage{
 	public static void save(byte[] input) throws Exception{
+		String face="-";
 		String key=System.getenv("firebase");
 		FileOutputStream fos=new FileOutputStream(new File("src/main/resources/static/smartb-7cffa-firebase-adminsdk-y293y-f7d6eeec7a.json"));
 		fos.write(key.getBytes());
@@ -21,8 +23,36 @@ class Storage{
 		    .setStorageBucket("smartb-7cffa.appspot.com")
 		    .build();
 		FirebaseApp.initializeApp(options);
+		String timestamp=(new Date()).toString();
 
 		Bucket bucket = StorageClient.getInstance().bucket();
-		bucket.create((new Date()).toString()+".jpeg",input);
+		bucket.create(timestamp+".jpeg",input);
+
+		FirebaseOptions dbOptions = new FirebaseOptions.Builder()
+			    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+			    .setDatabaseUrl("https://smartb-7cffa.firebaseio.com")
+			    .build();
+
+			boolean hasBeenInitialized=false;
+			List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
+			for(FirebaseApp app : firebaseApps){
+    			if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)){
+        			hasBeenInitialized=true;
+        			//finestayApp = app;
+    			}
+			}		
+
+			if(!hasBeenInitialized) {
+				FirebaseApp.initializeApp(dbOptions);
+			}
+
+			
+
+			final FirebaseDatabase database = FirebaseDatabase.getInstance();
+			DatabaseReference ref = database.getReference("storage/");
+
+			DatabaseReference motionRef=ref.child(timestamp);
+			motionRef.setValueAsync(face);
+			
 	}
 }
